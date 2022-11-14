851f256ceac4215e2e645ed669b74ecc0d90dfee
R3DRVM <99007375+R3DRVM@users.noreply.github.com>
2022-11-13 20:17:41 -0800

    Update vote.ts

--- scripts/vote.ts
+++ scripts/vote.ts
@@ -1,36 +1,36 @@
-import { ethers } from "hardhat"
-import * as dotenv from "dotenv"
-import { TokenizedBallot__factory } from "../typechain-types"
+import { ethers } from "hardhat";
+import * as dotenv from "dotenv";
+import { MyToken__factory, TokenizedBallot__factory } from "../typechain-types";
 dotenv.config()
 
-
-const ballotAddress = "0x19b8C35cEB46D3F273d9b87d4AEd183037dA8251" // add the address of our TokenizedBallot from etherscan
-const proposalToVoteFor = 3 // index of the proposal to vote for
-const votes = 100 // amount of voting power to spend
+const tokenAddress = "0x350e655770e02e05B4f22169A7b8d3d5aAd6B46a"; // Mytoken Address
+const ballotAddress = "0xf185087be41f7ae83690998f97c8c512b3a55f00"; // TokenizedBallot address
+const proposalN = 0; //  proposal to vote for
+const votePower = 1; // voting power used 
 
 async function main() {
     const provider = ethers.getDefaultProvider("goerli", { alchemy: process.env.ALCHEMY_API })
     const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC ?? "");
     const signer = wallet.connect(provider);
-    const ballotContractFactory = new TokenizedBallot__factory(signer)
-    const ballotContract = ballotContractFactory.attach(ballotAddress)
-
-    console.log("Checking your voting power")
-    const votingPower = await ballotContract.votingPower(signer.address)
+    const tokenContractFactory = new MyToken__factory(signer);
+    const tokenContract = tokenContractFactory.attach(tokenAddress)
+    const ballotContractFactory = new TokenizedBallot__factory(signer);
+    const ballotContract = ballotContractFactory.attach(ballotAddress);
+    const votingPower = await ballotContract.votingPower(signer.address);
+        console.log("Checking voting power");
     if (votingPower.eq(0)) {
-        console.log("You do not have voting power")
-        return
+         return       
+        console.log("You do not have voting power");
     } else {
-        console.log(`You have ${votingPower} votes left`)
+        console.log(`You have ${votingPower} votes left`);
     }
-    console.log(`Voting for proposal #${proposalToVoteFor}...`)
+    console.log(`Voting for proposal #${proposalN}`);
 
-    const voteTx = await ballotContract.vote(proposalToVoteFor, votes)
-    await voteTx.wait()
-    console.log("Thank you for voting!")
-    const proposalVoteCount = await (await ballotContract.proposals(proposalToVoteFor)).voteCount
-
-    console.log(`Your chosen proposal now has ${proposalVoteCount} votes!`)
+    const voteTx = await ballotContract.vote(proposalN, votePower);
+    await voteTx.wait();
+    console.log("Thanks for voting!");
+    const proposalVoteCount = await (await ballotContract.proposals(proposalN)).voteCount;
+    console.log(`Your proposal now has ${proposalVoteCount} votes`);
 }
 
 main().catch((error) => {
